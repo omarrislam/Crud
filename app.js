34c42509ce60b63f4e13a15b486a0dd93125e51c
mohamed <m6abdelmonsef@gmail.com>
2021-09-17 16:41:30 +0200

    test num 5

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const app = express()
 var cors = require('cors')
-const port = 3000 || process.env.PORT
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://admin:admin@cluster0.srnvy.mongodb.net/brilliant-DB', { useNewUrlParser: true, useUnifiedTopology: true })
-.then(()=>{
-    console.log('Server connected successfully');
-}).catch((err)=>{
-    console.log('error in db ====>>> ' ,err);
-})
+
+
 app.use(cors())
 app.use(express.json())
 app.use(require('./routes/register.routes'))
 app.use(require('./routes/login.routes'))
 app.use(require('./routes/home.routes'))
 
-app.listen(port, () => console.log("Connected"))
+mongoose.connect('mongodb+srv://admin:admin@cluster0.srnvy.mongodb.net/brilliant-DB',
+ {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false }).then(()=>{
+     console.log('DB===Connect');
+ }).catch(()=>{
+     console.log('DB ERoR');
+ })
+app.listen(3000 || process.env.PORT, () =>{
+    console.log("Connected")
+})
 
 
 
